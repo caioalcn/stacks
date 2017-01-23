1654c6f57ebe31a58f09f0b8f77f7f41d74cdde9
Caio Alcântara <caiocomputers@gmail.com>
2017-01-23 11:51:16 -0200

    Initial Commit

---
+++ stackview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackview
+//
+//  Created by Caio Alcântara on 23/01/2017.
+//  Copyright © 2017 Caio Alcântara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
